a5aeeeccbbfddf4faa4ed1d15b704a653c2f8af7
Matthew Palmer <mattjpalmer92@gmail.com>
2021-11-24 15:08:41 +0000

    ammended async await

---
+++ assets/js/async-await.js
@@ -0,0 +1,26 @@
+// declare an asynchronous function
+const myFirstAsyncFn = async function (url) {
+  // make a network call here
+  const response = await fetch(url);
+
+  console.log(response.status);
+
+  if (response.status !== 200) {
+    return { error: "Something went wrong" };
+  } else {
+    // use the .json() to extract the data from the response object
+    const data = await response.json();
+
+    return data;
+  }
+};
+
+const init = async function () {
+  const result = await myFirstAsyncFn(
+    "https://api.openweathermap.org/data/2.5/weather?q=Leeds&appid=393609ac7b2e5f25ccdd00e626ee13ddx"
+  );
+
+  console.log(result);
+};
+
+init();
